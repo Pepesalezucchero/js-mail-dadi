5ae29be4f4b5dffc84d20f7b954f1b78c4cd0bcc
Alessio Rugi <alessiorugi95@gmail.com>
2023-11-22 16:40:56 +0100

    js: inserito array

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,3 +1,8 @@
 /*Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 */
+
+//inserisco delle email che sono già presenti nella lista per accedere
+
+const userSubscribe = ["pinoilfagiolino@gmail.com", "milanochebellamano@gmail.com", "ginalapaperina@gmail.com", "dulcisinfundo@hotmail.it"];
+console.log(userSubscribe);
