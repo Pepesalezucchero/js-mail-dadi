56e1d1a256cb640be63ede6a3aa52a21821d4f16
Alessio Rugi <alessiorugi95@gmail.com>
2023-11-22 17:58:16 +0100

    js: inseriti elementi iniziali e definito vittoria

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -1,3 +1,22 @@
 /*Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
+
+//generiamo dei numeri casuali, per il giocatore e per il computer
+let userNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Numero uscito all'utente", userNumber);
+
+let pcNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Numero uscito al pc", pcNumber);
+
+//stabiliamo il vincitore confrontando i numeri dei dadi ottenuti, il più alto vince
+
+if (userNumber > pcNumber) {
+    alert("Hai vinto!");
+
+} else if (userNumber === pcNumber) {
+    alert("Parità");
+
+} else {
+    alert("Sconfitta :(");
+}
